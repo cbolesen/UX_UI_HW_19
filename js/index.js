5d87ab2c4ad2018175af3609b8180156f1f87b98
Chandy Olesen <cbolesen@yahoo.com>
2021-01-29 09:54:44 -0700

    Link to DIY Case Study
    Linked DIY image to DIY case study

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 console.log("Your index.js file is loaded correctly!");
-$(document).ready(function(){
-    $("#workGrid").hover(function(){
+  $(document).ready(function(){
+    $(".heroImg").hover(function(){
         alert("Welcome to my work!");
     },
     function(){
